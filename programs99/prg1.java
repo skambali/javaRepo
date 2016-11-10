0f9604f6a378ce6481e9619735a1396aa0a7820d
Anoop P S <Anoop P S@DESKTOP-P5GFM38>
2016-11-10 21:52:09 +0530

    MyJavaProject

---
+++ programs99/prg1.java
@@ -0,0 +1,16 @@
+package programs99;
+// to read a string from user
+
+import java.util.Scanner;
+
+public class prg1 {
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a string value:");
+		String str=sc.next();
+		System.out.print("The entered string is:"+str);
+		sc.close();
+	}
+}
